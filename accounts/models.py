ceafcf9a859c0ceb207e291f1199aa4215391b36
sdah24@gmail.com <sdah24@gmail.com>
2025-10-04 03:16:30 +0600

    question model,urls and quiz html page are updated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.db.models import Model
+
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.TextField()
+    opt_a = models.CharField(max_length=240)
+    opt_b = models.CharField(max_length=240)
+    opt_c = models.CharField(max_length=240)
+    opt_d = models.CharField(max_length=240)
+    correct_option = models.CharField(max_length=1, choices=[
+        ("A", "Option A"),
+        ("B", "Option B"),
+        ("C", "Option C"),
+        ("D", "Option D"),
+    ])
+
+    def __str__(self):
+        return self.question_text[:500]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Question
 # Create your views here.
+
+def questions(request):
+    q = Question.objects.order_by('?')[:5]
+    return render(request,'account_quiz.html',{"questions":q})
