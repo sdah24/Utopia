fdd75497262c78fae60fed16ae845a03505f4b0d
sdah24@gmail.com <sdah24@gmail.com>
2025-10-04 23:50:45 +0600

    profile model creation done

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 from django.db.models import Model
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    display_name = models.CharField(max_length=150, blank=True)
+    bio = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    quiz_score = models.IntegerField(null=True, blank=True)    # percent or raw
+    badge = models.CharField(max_length=20, blank=True, null=True)
+
+
+    def __str__(self):
+        return self.user.username
+
 
 class Question(models.Model):
     question_text = models.TextField()
