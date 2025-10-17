fa0f14b81488dc4076f0a5619b5c69135de71948
Syed Alif Hasan <sdah24@gmail.com>
2025-10-17 23:00:03 +0600

    final touch before submission

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile, name="profile"),
-path('people/', views.people_list, name='people_list'),
+    path('people/', views.people_list, name='people_list'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,10 @@
 import random
 from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required
+
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.contrib.auth.models import User
+
 from .forms import UserRegisterForm
 from .models import Question
-
 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
